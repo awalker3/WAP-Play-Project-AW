a088962f4305a7c6f899930f4bbf31709a06da72
Alex Walker - PERSONAL <alexnwalker@protonmail.com>
2021-04-06 03:35:38 -0500

    play videos through 17 working with one error

--- server/app/controllers/TaskList1.scala
+++ server/app/controllers/TaskList1.scala
@@ -2,11 +2,95 @@ package controllers
 
 import javax.inject._
 import play.api.mvc._
+import models.TaskListInMemoryModel
+import play.api.data._
+import play.api.data.Forms._
+
+case class LoginData(username: String, password: String)
 
 @Singleton
-class TaskList1 @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  def taskList = Action {
-      val tasks = List("task1", "task2", "task3", "task4")
-      Ok(views.html.taskList1(tasks))
+class TaskList1 @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
+  val loginForm = Form(mapping(
+    "Username" -> text(3, 10),
+    "Password" -> text(8)
+    )(LoginData.apply)(LoginData.unapply))
+  
+    def login = Action { implicit request =>
+        Ok(views.html.login1(loginForm))
+    }
+
+    def validateLoginGet (username: String, password: String) = Action {
+      Ok(s"You have logged in with $username and $password")
+    }
+
+    def validateLoginPost = Action { implicit request => 
+      val postVals = request.body.asFormUrlEncoded
+      postVals.map {args => 
+        val username = args("username").head
+        val password = args("password").head
+        if(TaskListInMemoryModel.validateUser(username, password))
+          Redirect(routes.TaskList1.taskList()).withSession("username" -> username)
+        else Redirect(routes.TaskList1.login()).flashing("error" -> "Incorrect username or password")
+      }.getOrElse(Redirect(routes.TaskList1.login()))
+    }
+
+     def createUserForm = Action { implicit request =>
+    loginForm.bindFromRequest().fold(
+      formWithErrors => BadRequest(views.html.login1(formWithErrors)),
+      ld =>
+        if (TaskListInMemoryModel.createUser(ld.username, ld.password)) {
+          Redirect(routes.TaskList1.taskList()).withSession("username" -> ld.username)
+        } else {
+          Redirect(routes.TaskList1.login()).flashing("error" -> "User creation failed.")
+        })
   }
+  
+    def taskList = Action { implicit request =>
+      val usernameOption = request.session.get("username")
+      usernameOption.map { username =>
+        val tasks = TaskListInMemoryModel.getTasks(username)
+        Ok(views.html.taskList1(tasks))
+      }.getOrElse(Redirect(routes.TaskList1.login()))
+      
+    }
+
+    def createUser = Action { request => 
+      val postVals = request.body.asFormUrlEncoded
+      postVals.map {args => 
+        val username = args("username").head
+        val password = args("password").head
+        if(TaskListInMemoryModel.createUser(username, password))
+          Redirect(routes.TaskList1.taskList()).withSession("username" -> username)
+        else Redirect(routes.TaskList1.login()).flashing("error" -> "User creation failed")
+      }.getOrElse(Redirect(routes.TaskList1.login()))
+    }
+
+    def logout = Action {
+      Redirect(routes.TaskList1.login()).withNewSession
+    }
+
+    def addTask = Action {implicit request =>
+      val usernameOption = request.session.get("username")
+      usernameOption.map { username =>
+        val postVals = request.body.asFormUrlEncoded
+        postVals.map {args => 
+          val task = args("newTask").head
+          TaskListInMemoryModel.addTask(username, task)
+          Redirect(routes.TaskList1.taskList())
+        }.getOrElse(Redirect(routes.TaskList1.taskList()))
+      }.getOrElse(Redirect(routes.TaskList1.login()))
+    }
+
+    def deleteTask = Action {implicit request =>
+      val usernameOption = request.session.get("username")
+      usernameOption.map { username =>
+        val postVals = request.body.asFormUrlEncoded
+        postVals.map {args => 
+          val task = args("index").head.toInt
+          TaskListInMemoryModel.removeTask(username, task)
+          Redirect(routes.TaskList1.taskList())
+        }.getOrElse(Redirect(routes.TaskList1.taskList()))
+      }.getOrElse(Redirect(routes.TaskList1.login()))
+    }
+
 }
---
+++ server/app/models/TaskListInMemoryModel.scala
@@ -0,0 +1,33 @@
+package models
+import collection.mutable
+
+object TaskListInMemoryModel {
+    private val users = mutable.Map[String, String]("mark" -> "pass")
+    private val tasks = mutable.Map[String, List[String]]("mark" -> List("eat", "sleep", "code"))
+    
+    def validateUser (username: String, password: String): Boolean = {
+        users.get(username).map(_ == password).getOrElse(false)
+    }
+
+    def createUser(username: String, password: String): Boolean = {
+        if(users.contains(username)) return false
+        else {
+            users(username) = password
+            return true
+        }
+    }
+
+    def getTasks(username: String): Seq[String] = {
+        tasks.get(username).getOrElse(Nil)
+    }
+
+    def addTask(username: String, task: String): Unit = {
+        tasks(username) = task :: tasks.get(username).getOrElse(Nil)
+    }
+
+    def removeTask(username: String, index: Int): Boolean = {
+        if(index < 0 || tasks.get(username).isEmpty || index >= tasks(username).length) false
+        tasks(username) = tasks(username).patch(index, Nil, 1)
+        true
+    }
+}
