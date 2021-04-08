28f16c56b35464d12bb0e24849e1294e6fd0178b
Alex Walker <awalker3@trinity.edu>
2021-04-07 21:27:15 -0500

    Finished the videos up until Ajax calls, so I have enough knowledge to do tasks 5 and 6

--- server/test/TaskList1Spec.scala
+++ server/test/TaskList1Spec.scala
@@ -7,13 +7,22 @@ import controllers.TaskList1
 
 class TaskList1Spec extends PlaySpec with GuiceOneServerPerSuite with OneBrowserPerSuite with HtmlUnitFactory {
     "Task list 1" must {
-    "login and see task list" in {
-      go to s"localhost:$port/login1"
+    "show login page" in {
+      go to s"http://localhost:$port/login1"
+      pageTitle mustBe "Login"
+      find(cssSelector("h1")).isEmpty mustBe false
+      find(cssSelector("h1")).foreach(e => e.text mustBe "Login")
       click on "username-login"
       textField("username-login").value = "mark"
       click on "password-login"
-      pwdField(id("password-login".stripMargin)).value = "pass"
+      textField(id("password-login")).value = "pass"
       submit()
+    }
+    "go to task list page" in {
+      pageTitle mustBe "Task List"
+      find(cssSelector("h1")).isEmpty mustBe false
+      find(cssSelector("h1")).foreach(e => e.text mustBe "Task List")
+      findAll(cssSelector("li")).toList.map(_.text) mustBe List("eat", "sleep", "code")
     }
     }
 }
